6e65dde856710fde5102a682f3b702eabdd9533f
jameelshakrna-max <jameelshakrna@gmail.com>
2026-04-11 21:20:48 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  reactStrictMode: false,
+  serverExternalPackages: ["@prisma/client"],
 };
 
 export default nextConfig;
